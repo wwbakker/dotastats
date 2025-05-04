0775f06bdb266093b6062cbad75fdb56472479c7
Wessel W. Bakker <wwbakker@gmail.com>
2025-05-04 16:00:09 +0200

    - Updated the players.

--- src/main/scala/nl/wwbakker/services/dota/statistics/model/Players.scala
+++ src/main/scala/nl/wwbakker/services/dota/statistics/model/Players.scala
@@ -3,11 +3,13 @@ package nl.wwbakker.services.dota.statistics.model
 object Players {
 
   val wesselId = 21842016
-  val beamcannon = 56624400
 //  val buzzKillington = 66268281
-  val chairmanId = 55277122
+//  val chairmanId = 55277122
   //  private val nikolaId = 39067906
   val jan = 22595249
+  val jens = 38355389
+  val norbert = 56624400
+  val stefan = 89572727
 
-  val everyone = Seq(wesselId, chairmanId, beamcannon, jan)
+  val everyone: Seq[Int] = Seq(wesselId, norbert, jan, jens, stefan)
 }
